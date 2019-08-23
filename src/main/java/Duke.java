85aab2395d176ec740c68ad2a6975f323c50a1e8
andrewleow97 <e0323960@u.nus.edu>
2019-08-23 17:05:26 +0800

    Week 2 Level 1: Greet, Echo Exit
    Signed-off-by: andrewleow97 <e0323960@u.nus.edu>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
 public class Duke {
+    private static void printIndent() {
+        out.print("    ");
+    }
+    private static void printIndentLine() {
+        out.println("    ____________________________________________________________");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                printIndentLine();
+                printIndent();
+                out.println("Bye. Hope to see you again soon!");
+                printIndentLine();
+                break;
+            } else {
+                printIndentLine();
+                printIndent();
+                out.println(input);
+                printIndentLine();
+            }
+        }
     }
 }
